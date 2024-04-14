03c479e514fd80bfabbc6d72344f0a5f89bc5989
Evgeny Vinnik <evgenyvinnik@ya.ru>
2024-04-13 22:58:26 -0700

    added basic scheme

--- main.js
+++ main.js
@@ -1,1 +1,92 @@
 import * as THREE from 'three';
+
+import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls.js';
+
+import starsTexture from './img/stars.jpg';
+import sunTexture from './img/sun.jpg';
+import earthTexture from './img/earth.jpg';
+import moonTexture from './img/moon.jpg';
+
+const renderer = new THREE.WebGLRenderer();
+
+renderer.setSize(window.innerWidth, window.innerHeight);
+
+document.body.appendChild(renderer.domElement);
+
+const scene = new THREE.Scene();
+
+const camera = new THREE.PerspectiveCamera(
+    45,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    1000
+);
+
+const orbit = new OrbitControls(camera, renderer.domElement);
+
+camera.position.set(-90, 140, 140);
+orbit.update();
+
+const ambientLight = new THREE.AmbientLight(0x666666);
+scene.add(ambientLight);
+
+const cubeTextureLoader = new THREE.CubeTextureLoader();
+scene.background = cubeTextureLoader.load([
+    starsTexture,
+    starsTexture,
+    starsTexture,
+    starsTexture,
+    starsTexture,
+    starsTexture
+]);
+
+const textureLoader = new THREE.TextureLoader();
+
+const sunGeo = new THREE.SphereGeometry(16, 30, 30);
+const sunMat = new THREE.MeshBasicMaterial({
+    map: textureLoader.load(sunTexture)
+});
+const sun = new THREE.Mesh(sunGeo, sunMat);
+scene.add(sun);
+
+function createPlanete(size, texture, position, ring) {
+    const geo = new THREE.SphereGeometry(size, 30, 30);
+    const mat = new THREE.MeshStandardMaterial({
+        map: textureLoader.load(texture)
+    });
+    const mesh = new THREE.Mesh(geo, mat);
+    const obj = new THREE.Object3D();
+    obj.add(mesh);
+
+    scene.add(obj);
+    mesh.position.x = position;
+    return {mesh, obj}
+}
+
+
+const earth = createPlanete(6, earthTexture, 62);
+const moon = createPlanete(4, moonTexture, 78);
+
+const pointLight = new THREE.PointLight(0xFFFFFF, 2, 300);
+scene.add(pointLight);
+
+function animate() {
+    //Self-rotation
+    sun.rotateY(0.004);
+    earth.mesh.rotateY(0.02);
+    moon.mesh.rotateY(0.018);
+
+    //Around-sun-rotation
+    earth.obj.rotateY(0.01);
+    moon.obj.rotateY(0.008);
+
+    renderer.render(scene, camera);
+}
+
+renderer.setAnimationLoop(animate);
+
+window.addEventListener('resize', function() {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+});
